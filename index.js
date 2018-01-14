b20020bc29a9f8fdae45dcf93660652273f43d5a
David Clark <dlc.clark@gmail.com>
2018-01-14 02:58:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(){
+  kittens.push('Ralph');
+}
+
+function destructivelyPrependKitten(){
+  kittens.unshift('Bob');
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
